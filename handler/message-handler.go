6681a10c37dca28150f19f643672d77a594e47fa
TwinProduction <christian-ct@hotmail.com>
2018-11-16 15:15:55 -0500

    Aligned anonymous function with other fields

--- handler/message-handler.go
+++ handler/message-handler.go
@@ -22,7 +22,7 @@ var commands = map[string]CommandInfo {
 	"shrug": {
 		category:    "misc",
 		description: "¯\\_(ツ)_/¯",
-		Execute: func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
+		Execute:     func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
 			_, e := b.ChannelMessageSend(m.ChannelID, m.Author.Mention()+": ¯\\_(ツ)_/¯")
 			if e != nil {
 				return false
@@ -33,7 +33,7 @@ var commands = map[string]CommandInfo {
 	"whoami": {
 		category:    "misc",
 		description: "Replies with the username of the user followed by the discriminator, e.g. `Twin#9089`.",
-		Execute: func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
+		Execute:     func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
 			b.ChannelMessageSend(m.ChannelID, m.Author.Username + "#" + m.Author.Discriminator)
 			return true
 		},
@@ -41,7 +41,7 @@ var commands = map[string]CommandInfo {
 	"pat": {
 		category:    "roleplay",
 		description: "Sends a GIF or an image of a character patting the head of another character",
-		Execute: func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
+		Execute:     func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
 			roleplay.Pat(b, m)
 			return true
 		},
@@ -49,7 +49,7 @@ var commands = map[string]CommandInfo {
 	"hug": {
 		category:    "roleplay",
 		description: "Sends a GIF or an image of a character hugging another character",
-		Execute: func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
+		Execute:     func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
 			roleplay.Hug(b, m)
 			return true
 		},
@@ -57,7 +57,7 @@ var commands = map[string]CommandInfo {
 	"greet": {
 		category:    "roleplay",
 		description: "Sends a GIF or an image of a character greeting another character",
-		Execute: func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
+		Execute:     func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
 			roleplay.Greet(b, m)
 			return true
 		},
@@ -65,28 +65,28 @@ var commands = map[string]CommandInfo {
 	"youtube": {
 		category:    "search",
 		description: "Returns the top YouTube search results for the given query",
-		Execute: func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
+		Execute:     func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
 			return search.SearchHandler(b, m, cmd, query)
 		},
 	},
 	"google": {
 		category:    "search",
 		description: "Returns the top Google search results for the given query",
-		Execute: func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
+		Execute:     func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
 			return search.SearchHandler(b, m, cmd, query)
 		},
 	},
 	"urban": {
 		category:    "Returns the UrbanDictionary definition of the given query",
 		description: "¯\\_(ツ)_/¯",
-		Execute: func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
+		Execute:     func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
 			return search.SearchHandler(b, m, cmd, query)
 		},
 	},
 	"purge": {
 		category:    "moderation",
 		description: "Removes N messages from the current channel",
-		Execute: func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
+		Execute:     func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
 			moderation.Purge(b, m, query)
 			return true
 		},
@@ -94,7 +94,7 @@ var commands = map[string]CommandInfo {
 	"blacklist": {
 		category:    "moderation",
 		description: "Manages blacklisted users",
-		Execute: func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
+		Execute:     func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
 			if len(arguments) != 3 {
 				util.SendErrorMessage(b, m, "**USAGE:** `" + Constants.COMMAND_PREFIX + "blacklist <add|remove> <userId>`")
 				return false
@@ -106,7 +106,7 @@ var commands = map[string]CommandInfo {
 	"perms": {
 		category:    "moderation",
 		description: "Manages permissions",
-		Execute: func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
+		Execute:     func(b *discordgo.Session, m *discordgo.MessageCreate, cmd string, query string, arguments []string) bool {
 			if len(arguments) != 4 {
 				util.SendErrorMessage(b, m, "**USAGE:** `" + Constants.COMMAND_PREFIX + "perms <add|remove> <cmd> <userId>`")
 				return false
