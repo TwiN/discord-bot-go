2ffecec056335c5aea1c938c6b95a25a1284aa84
TwinProduction <twin@twinnation.org>
2018-12-08 21:53:40 -0500

    Work on color roles

--- role/color-initializer.go
+++ role/color-initializer.go
@@ -1,13 +1,15 @@
 package role
 
 func Initialize() {
-
+	createColorChannelIfNotExist()
+	createColorRolesIfNotExist()
 }
 
 func createColorChannelIfNotExist() {
-
+	// TODO: Check if channel exists
+	// TODO: Create channel if it doesn't exist
 }
 
 func createColorRolesIfNotExist() {
-
+	// TODO: Check if each roles exist recursively, and create a role when it doesn't exist
 }
